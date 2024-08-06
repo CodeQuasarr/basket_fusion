e011f12b1762620fcfc5a908389174fe49db0bb3
Samuel ANYARONKE <dsanyaronke@gmail.com>
2024-08-06 06:56:03 +0200

    #1 Ajout du champs role  avec les relations correspondantes dans les modèles `User`

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Clubs\Team;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,10 +14,21 @@ use Laravel\Sanctum\HasApiTokens;
 /**
  * @property string $name
  * @property string $email
+ * @property string $password
+ * @property string $weight
+ * @property string $height
+ * @property string $birth_date
+ * @property int $number
+ * @property int $role
  */
 class User extends Authenticatable
 {
     use HasFactory, Notifiable, HasApiTokens, softDeletes;
+
+
+    public const ADMIN = 99;
+    public const COACH = 2;
+    public const PLAYER = 1;
 
     /**
      * The attributes that are mass assignable.
@@ -23,7 +36,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name', 'email', 'password', 'weight', 'height', 'birth_date', 'number', 'created_at', 'updated_at', 'deleted_at'
+        'role', 'name', 'email', 'password', 'weight', 'height', 'birth_date', 'number', 'created_at', 'updated_at', 'deleted_at'
     ];
 
     /**
@@ -49,7 +62,83 @@ class User extends Authenticatable
         ];
     }
 
+    /**
+     * @description The attributes that should be mutated to dates.
+     * @return void
+     */
+    public static function boot(): void
+    {
+        parent::boot();
 
+        static::addGlobalScope('order', function ($builder) {
+            $builder->orderBy('name', 'asc');
+        });
+    }
+
+    /**************************************************************************
+     * Relationships
+     **************************************************************************/
+
+    /**
+     * @description Get the players that the coach has
+     * @return BelongsToMany
+     */
+    public function players(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'coaches_players', 'coach_id', 'player_id');
+    }
+
+    /**
+     * @description Get the coach of the player
+     * @return BelongsToMany
+     */
+    public function Coach(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'coaches_players', 'player_id', 'coach_id');
+    }
+
+    /**
+     * @description Get the teams that the user belongs to
+     * @return BelongsToMany
+     */
+    public function teams(): BelongsToMany
+    {
+        return $this->belongsToMany(Team::class, 'user_teams', 'user_id', 'team_id');
+    }
+
+
+    /**************************************************************************
+     * Methods
+     **************************************************************************/
+
+    /**
+     * @description Get the role name
+     * @return string
+     */
+    public function getRoleName(): string
+    {
+        return match ($this->role) {
+            self::ADMIN => 'Admin',
+            self::COACH => 'Coach',
+            self::PLAYER => 'Player',
+            default => 'Unknown',
+        };
+    }
+
+    /**
+     * @description Check if the user has a specific role
+     * @param int $role
+     * @return bool
+     */
+    public function hasRole(int $role): bool
+    {
+        return $this->role === $role;
+    }
+
+    /**
+     * @description method to get the user name
+     * @return string
+     */
     public function getName(): string
     {
         return $this->name;
